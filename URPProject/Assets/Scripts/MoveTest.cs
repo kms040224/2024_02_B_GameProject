6ac63fda6a91a53419bfddeb4f652ac9d167193a
kms040224 <127456115+kms040224@users.noreply.github.com>
2024-09-05 10:00:18 +0900

    Update MoveTest.cs

--- URPProject/Assets/Scripts/MoveTest.cs
+++ URPProject/Assets/Scripts/MoveTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveTest : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform MoveOffset;
     void Start()
     {
         
