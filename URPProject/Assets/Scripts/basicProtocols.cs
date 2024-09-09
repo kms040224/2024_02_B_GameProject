b11122a067cb2b1f83495a0314d55ce83cbc87b5
kms040224 <kms0402246103@gmail.com>
2024-09-09 15:40:50 +0900

    [Feat] Node.js와 Unity3D WEB 통신 Get까지

---
+++ URPProject/Assets/Scripts/basicProtocols.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicProtocols : MonoBehaviour
+{
+    public class Pockets
+    {
+
+        public class res
+        {
+            public string message;
+        }
+    }
+}
